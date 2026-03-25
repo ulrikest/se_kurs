f5abd1967e55486ebfa0a2536d200efd8c1af478
Frank <julie.frank@dlr.de>
2026-03-25 11:28:37 +0100

    changed from global variables to class definition

--- uli_vicsek/src/vicsek/vicsek_bad.py
+++ uli_vicsek/src/vicsek/vicsek_bad.py
@@ -1,82 +1,91 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-import time
-import math
-import click
 import logging
+import click
+
+class VicsekModel:
+    def __init__(self, n, d, v, dt, eta):
+        self.n = n  # Number of points
+        self.d = d  # Distance between points
+        self.v = v  # Velocity of points
+        self.dt = dt  # Time step
+        self.eta = eta  # Noise
+        
+        # Initialize the positions and angles
+        self.r = np.random.random((self.n, 2))
+        self.theta = np.random.random(self.n)
+        self.counter = 0
+
+    def distance(self, p1, p2):
+        return np.sqrt(((p1 - p2) ** 2).sum())
+
+    def update_model(self):
+        for i in range(self.n):
+            sum_sin = 0
+            sum_cos = 0
+            neighbours = 0
+
+            for j in range(self.n):
+                if i != j:
+                    if self.distance(self.r[i], self.r[j]) < self.d:
+                        theta_j = 2 * np.pi * self.theta[j]
+                        sum_sin += np.sin(theta_j)
+                        sum_cos += np.cos(theta_j)
+                        neighbours += 1
+
+            if neighbours > 0:
+                avg_theta = np.arctan2(sum_sin / neighbours, sum_cos / neighbours)
+                self.theta[i] = (avg_theta / (2 * np.pi)) + self.eta * (np.random.rand() - 0.5)
+
+            dx = self.v * self.dt * np.cos(2 * np.pi * self.theta[i])
+            dy = self.v * self.dt * np.sin(2 * np.pi * self.theta[i])
+
+            self.r[i, 0] = self.r[i, 0] + dx
+            self.r[i, 1] = self.r[i, 1] + dy
+
+            # Boundary conditions
+            if self.r[i, 0] > 1:
+                self.r[i, 0] = 0
+            if self.r[i, 1] > 1:
+                self.r[i, 1] = 0
+            if self.r[i, 0] < 0:
+                self.r[i, 0] = 1
+            if self.r[i, 1] < 0:
+                self.r[i, 1] = 1
+
+            self.counter += 1
+
+    def animate(self, frame, q):
+        self.update_model()
+
+        x = self.r[:, 0]
+        y = self.r[:, 1]
+        u = np.cos(2 * np.pi * self.theta)
+        vv = np.sin(2 * np.pi * self.theta)
+
+        q.set_offsets(np.c_[x, y])
+        q.set_UVC(u, vv)
+
+        print("frame", frame, "counter", self.counter)
+        return q,
+
+    def run(self):
+        fig, ax = plt.subplots(figsize=(6, 6))
+        x = self.r[:, 0]
+        y = self.r[:, 1]
+        u = np.cos(2 * np.pi * self.theta)
+        vv = np.sin(2 * np.pi * self.theta)
+
+        q = ax.quiver(x, y, u, vv, angles='xy')
+        ax.set_xlim(0, 1)
+        ax.set_ylim(0, 1)
+        ax.set_title("Vicsek Model")
+
+        ani = FuncAnimation(fig, self.animate, frames=200, fargs=(q,), interval=50, blit=True)
+        plt.show()
 
 
-plt.rcParams['animation.embed_limit'] = 300
-#testnderung
-
-
-
-def distance(p1, p2):
-    return np.sqrt(((p1 - p2) ** 2).sum())
-
-
-def update_model():
-    global r, theta, counter
-
-    for i in range(n):
-        sum_sin = 0
-        sum_cos = 0
-        neighbours = 0
-
-        for j in range(n):
-            if i != j:
-                if distance(r[i], r[j]) < d:
-                    theta_j = 2 * np.pi * theta[j]
-                    sum_sin = sum_sin + np.sin(theta_j)
-                    sum_cos = sum_cos + np.cos(theta_j)
-                    neighbours = neighbours + 1
-
-        if neighbours > 0:
-            avg_theta = np.arctan2(sum_sin / neighbours, sum_cos / neighbours)
-            theta[i] = (avg_theta / (2 * np.pi)) + eta * (np.random.rand() - 0.5)
-
-        dx = v * dt * np.cos(2 * np.pi * theta[i])
-        dy = v * dt * np.sin(2 * np.pi * theta[i])
-
-        r[i, 0] = r[i, 0] + dx
-        r[i, 1] = r[i, 1] + dy
-
-        if r[i, 0] > 1:
-            r[i, 0] = 0
-        if r[i, 1] > 1:
-            r[i, 1] = 0
-        if r[i, 0] < 0:
-            r[i, 0] = 1
-        if r[i, 1] < 0:
-            r[i, 1] = 1
-
-        counter = counter + 1
-
-
-def animate(frame):
-    global q
-
-    update_model()
-
-    x = []
-    y = []
-    u = []
-    vv = []
-
-    for i in range(n):
-        x.append(r[i, 0])
-        y.append(r[i, 1])
-        u.append(np.cos(2 * np.pi * theta[i]))
-        vv.append(np.sin(2 * np.pi * theta[i]))
-
-    q.set_offsets(np.c_[x, y])
-    q.set_UVC(u, vv)
-
-    print("frame", frame, "counter", counter)
-
-    return q,
-    
 @click.command()
 @click.option("--n", default=200, help="Set number of points")
 @click.option("--d", default=0.01, help="Set distance between points")
@@ -84,26 +93,13 @@ def animate(frame):
 @click.option("--dt", default=1, help="Set time step")
 @click.option("--eta", default=0.1, help="Set noise")    
 
-    
 def main(n, d, v, dt, eta):
-    
     logging.basicConfig(
-    filename="app.log",
-    level=logging.INFO
+        filename="app.log",
+        level=logging.INFO
     )
     logging.info("Saved to File")
     logging.info("Program Started")
-    
-    global r, theta, counter
-    global n_global, d_global, v_global, dt_global, eta_global
-    global q
-
-    # assign to globals used elsewhere
-    globals()['n'] = n
-    globals()['d'] = d
-    globals()['v'] = v
-    globals()['dt'] = dt
-    globals()['eta'] = eta
     
     logging.info(f"n = {n}")
     logging.info(f"d = {d}")
@@ -111,31 +107,11 @@ def main(n, d, v, dt, eta):
     logging.info(f"dt = {dt}")
     logging.info(f"eta = {eta}")
     
-    r = np.random.random((n, 2))
-    theta = np.random.random(n)
-    #plot
-    fig, ax = plt.subplots(figsize=(6, 6))
-
-    x = r[:, 0]
-    y = r[:, 1]
-    u = np.cos(2 * np.pi * theta)
-    vv = np.sin(2 * np.pi * theta)
-
-    q = ax.quiver(x, y, u, vv, angles='xy')
-    ax.set_xlim(0, 1)
-    ax.set_ylim(0, 1)
-    ax.set_title("Vicsek Model")
-
-    counter = 0
+    # Create the VicsekModel instance
+    vicsek_model = VicsekModel(n, d, v, dt, eta)
     
-    ani = FuncAnimation(fig, animate, frames=200, interval=50, blit=True)
-    plt.show()
-    
-    
+    # Run the simulation
+    vicsek_model.run()
+
 if __name__ == "__main__":
-    
     main()
-
-
-
-
